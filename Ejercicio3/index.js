290f360d023fddd050d0f641f36a4afb37f4d6d3
= <=>
2020-10-21 20:50:19 +0200

    canvis

---
+++ Ejercicio3/index.js
@@ -0,0 +1,19 @@
+function reaccion1 () {
+    let primero = document.getElementsByClassName("boton1");
+    alert("¡Has seleccionado el Botón 1!");
+}
+
+function reaccion2 () {
+    let segundo = document.getElementsByClassName("boton2");
+    alert("¡Has seleccionado el Botón 2!");
+}
+
+function reaccion3 () {
+    let tercero = document.getElementsByClassName("boton3");
+    alert("¡Has seleccionado el Botón 3!");
+}
+
+function reaccion4 () {
+    let cuarto = document.getElementsByClassName("boton4");
+    alert("¡Has seleccionado el Botón 4!");
+}
