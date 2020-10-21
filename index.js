51fff6fa93563e8555be74ca17cac6963880aa32
= <=>
2020-10-21 14:40:25 +0200

    canvis

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ const modolectura = 0;
 const modonormal = 1;
 
 let boton = document.querySelectorAll("button");
+let imagenbye = document.querySelector("img");
 boton [modolectura].addEventListener("click", botonModoLectura);
 boton [modonormal].addEventListener("click", botonModoNormal);
 
 function botonModoLectura() {
     let boton1 = document.getElementsByTagName ("body")[0];
-    boton1.style.backgroundColor = "grey";
+    boton1.style.backgroundColor = "black";
+    boton1.style.color = "white";
+    imagenbye.className = "noimagen";
 }
+
 function botonModoNormal() {
     let boton2 = document.getElementsByTagName ("body")[0];
     boton2.style.backgroundColor = "white";
+    imagenbye.className = "imagen";
 }
 
